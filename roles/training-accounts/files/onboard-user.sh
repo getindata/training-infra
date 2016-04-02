e62c26c61b35a576dd5dc22d418bc22e758429d4
Adam Kawa <kawa.adam@gmail.com>
2016-04-02 19:01:35 +0200

    fixes after last run

--- roles/training-accounts/files/onboard-user.sh
+++ roles/training-accounts/files/onboard-user.sh
@@ -20,12 +20,14 @@ add-hdfs-account() {
         run-as-hdfs hadoop fs -chown ${user} /user/${user}
 }
 
+
 drop-user-hive-db-tables() {
 	user=$1
         run-as-hdfs hive -e "drop table if exists ${user}.stream;"
         run-as-hdfs hive -e "drop database if exists ${user};"
 	run-as-hdfs hadoop fs -rmr /apps/hive/warehouse/${user}.db /user/hive/warehouse/${user}.db
 }
+
 
 create-user-hive-db() {
 	user=$1
