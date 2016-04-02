4bfaa397aed1c8a3d28b6a7fad8632e5ea6232fc
Adam Kawa <kawa.adam@gmail.com>
2016-04-02 17:35:34 +0200

    cleanup of creation steps

--- one-command-cm-cluster.sh
+++ one-command-cm-cluster.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
 USERNAME=ec2-user
+PRIVATE_KEY=oregon.pem
+
 STEP=$1
 TERRAFORM_CONF_DIR=$2
-PRIVATE_KEY=$3
 
 TERRAFORM_PLAN=${TERRAFORM_CONF_DIR}/config.tf.plan
 TERRAFORM_STATE=${TERRAFORM_CONF_DIR}/terraform.tfstate
 
-if [[ ! -f oregon.pem ]] ; then
-    echo 'File "oregon.pem" is not there, aborting.'
+display_usage() { 
+    echo -e "\nUsage:\n$0 <step> <terraform-config-path> \n"
+} 
+
+if [  $# -le 1 ]; then 
+    display_usage
+    exit 1
+fi 
+
+if [[ ! -f ${PRIVATE_KEY} ]] ; then
+    echo 'File "${PRIVATE_KEY}" is not there, aborting.'
     exit
 fi
 
-terraform plan -out=${TERRAFORM_PLAN} ${TERRAFORM_CONF_DIR}
-terraform apply -state=${TERRAFORM_STATE} ${TERRAFORM_CONF_DIR}
+if [ "${STEP}" == "aws" ] ; then
+    terraform plan -out=${TERRAFORM_PLAN} ${TERRAFORM_CONF_DIR}
+    terraform apply -state=${TERRAFORM_STATE} ${TERRAFORM_CONF_DIR}
+fi
+
 ./get_aws_ips_from_terraform.py -p ${TERRAFORM_STATE}
 
 if [ "${STEP}" == "setup" ] ; then
@@ -24,9 +37,11 @@ if [ "${STEP}" == "setup" ] ; then
     ansible-playbook playbook/selinux-disable.yml -i hadoop_hosts -u ${USERNAME} --private-key ${PRIVATE_KEY}
 fi
 
-# sleep 4m
-
 if [ "${STEP}" == "cm" ] ; then
     ansible-playbook playbook/cm.yml -i hadoop_hosts -u ${USERNAME} --private-key ${PRIVATE_KEY}
     ansible-playbook playbook/cdh.yml -i hadoop_hosts -u ${USERNAME} --private-key ${PRIVATE_KEY}
+fi
+
+if [ "${STEP}" == "training" ] ; then
+    ansible-playbook playbook/training-setup.yml -i hadoop_hosts -u ${USERNAME} --private-key ${PRIVATE_KEY}
 fi
