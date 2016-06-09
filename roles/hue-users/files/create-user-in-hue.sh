e58afefc5ae304e494b9aa440062df1a4f1450e0
Adam Kawa <kawa.adam@gmail.com>
2016-06-09 20:44:22 +0200

    user create

--- roles/hue-users/files/create-user-in-hue.sh
+++ roles/hue-users/files/create-user-in-hue.sh
@@ -4,10 +4,12 @@ INSTALL_DIR="/usr/local/"
 
 USER=$1
 
+#/opt/cloudera/parcels/CDH/lib/hue/build/env/bin/hue shell <<EOF
 ${INSTALL_DIR}/hue/build/env/bin/hue shell <<EOF
 from django.contrib.auth.models import User
-user = User.objects.get(username='${USER}')
+user = User.objects.create(username='${USER}')
 user.set_password('${USER}')
 user.is_superuser = True
 user.save()
 EOF
+
