bd7f5a78db6d291146c10aa68b44f647ea3f13a4
root training <info@getindata.com>
2016-08-25 14:06:22 +0200

    add updates

--- roles/training-data/files/onboard_concerts.sh
+++ roles/training-data/files/onboard_concerts.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-mysqladmin -u root password root
-
 mysql -p'root' -e "create database if not exists streamrock"
 mysql -p'root' -e "drop table if exists streamrock.concert"
 mysql -p'root' -e "CREATE TABLE streamrock.concert \
                 (id INT NOT NULL PRIMARY KEY, artist VARCHAR(50) NOT NULL, day TIMESTAMP DEFAULT NOW(), city VARCHAR(50), location VARCHAR(25));"
 
-mysql -p'root' -e "load data local infile 'concerts.tsv' \
+mysql -p'root' -e "load data local infile '/tmp/concerts.tsv' \
                 into table streamrock.concert fields terminated by '\t' lines terminated by '\n' \
                 (id, artist, day, city, location)"
 
