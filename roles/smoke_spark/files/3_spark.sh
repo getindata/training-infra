6c0fb1bd8be6e0e9b037e934d1e57a3de00923b3
piotrektt <piotrek.tt@gmail.com>
2016-09-27 13:30:45 +0200

    Spark smoke test - commit 02 - testing

--- roles/smoke_spark/files/3_spark.sh
+++ roles/smoke_spark/files/3_spark.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-. common.sh
+. /tmp/common.sh
 
-spark-submit --master yarn-client --driver-memory 512m --executor-memory 256m --num-executors 3 --conf spark.ui.port=4062 spark-cmd-1.py
+spark-submit --master yarn-client --driver-memory 512m --executor-memory 256m --num-executors 3 --conf spark.ui.port=4062 /tmp/spark-cmd-1.py
 check_error_code
 
-spark-submit --master yarn-client --driver-memory 512m --executor-memory 256m --num-executors 3 --conf spark.ui.port=4062 spark-cmd-2.py
+spark-submit --master yarn-client --driver-memory 512m --executor-memory 256m --num-executors 3 --conf spark.ui.port=4062 /tmp/spark-cmd-2.py
 check_error_code
 
-spark-submit --master yarn-client --driver-memory 512m --executor-memory 256m --num-executors 3 --conf spark.ui.port=4062 spark-cmd-3.py
+spark-submit --master yarn-client --driver-memory 512m --executor-memory 256m --num-executors 3 --conf spark.ui.port=4062 /tmp/spark-cmd-3.py
 check_error_code
