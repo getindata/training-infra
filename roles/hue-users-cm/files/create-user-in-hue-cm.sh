ac97187d290b2ec10f7b9c1ed2da5a1dfd28fe9f
Adam Kawa <kawa.adam@gmail.com>
2018-11-23 15:37:55 +0100

    supporting existing users

--- roles/hue-users-cm/files/create-user-in-hue-cm.sh
+++ roles/hue-users-cm/files/create-user-in-hue-cm.sh
@@ -2,20 +2,19 @@
 
 
 INSTALL_DIR="/opt/cloudera/parcels/CDH/lib/"
-USER=$1
-PASSWORD_SUFFIX=$2
-HUE_DB_PASS=$3
+HUE_DB_PASS=$1
+USER=$2
+PASS_SUFFIX=$3
 
 export HUE_CONF_DIR="/var/run/cloudera-scm-agent/process/`ls -alrt /var/run/cloudera-scm-agent/process | grep HUE | tail -1 | awk '{print $9}'`"
 export HUE_IGNORE_PASSWORD_SCRIPT_ERRORS=1
 export HUE_DATABASE_PASSWORD=$HUE_DB_PASS
 
-#/opt/cloudera/parcels/CDH/lib/hue/build/env/bin/hue shell <<EOF
-${INSTALL_DIR}/hue/build/env/bin/hue shell <<EOF
+${INSTALL_DIR}/hue/build/env/bin/hue shell --cm-managed <<EOF
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
-user = User.objects.create(username='${USER}')
-user.set_password('${USER}${PASSWORD_SUFFIX}')
+user, created = User.objects.get_or_create(username='${USER}')
+user.set_password('${USER}${PASS_SUFFIX}')
 user.is_active = True
 user.is_superuser = True
 group_obj = Group.objects.get(name="default")
