2bba16d4576be854c18bcafd7b35a76ec992e4dd
Jakub Maleszewski <jakub.maleszewski@getindata.com>
2019-01-03 15:59:19 +0100

    Enable full automation

--- one-command-cm-cluster.sh
+++ one-command-cm-cluster.sh
@@ -9,5 +9,5 @@ set -e -x
 ./one-command-step.sh setup ${USERNAME} ${TERRAFORM_CONF_DIR}
 ./one-command-step.sh mysql ${USERNAME} ${TERRAFORM_CONF_DIR}
 ./one-command-step.sh cm ${USERNAME} ${TERRAFORM_CONF_DIR}
-#./one-command-step.sh training ${USERNAME} ${TERRAFORM_CONF_DIR}
+./one-command-step.sh training ${USERNAME} ${TERRAFORM_CONF_DIR}
 #./one-command-step.sh confluent ${USERNAME} ${TERRAFORM_CONF_DIR}
